e459729dc65fd537953385089559b7f2481ac2a5
niveditainfotech <neviditaneeacademy@gmail.com>
2024-05-09 21:18:27 +0530

    op

--- static/script.js
+++ static/script.js
@@ -8,3 +8,14 @@ function show(){
     bar.style.display='none'
     bar.style.transition='.5s'
 }
+
+
+
+let preloader=document.querySelector("[data-preaload]")
+
+setTimeout(loader,2000)
+
+function loader(){
+    preloader.classList.add("loaded")
+    document.body.classList.add("loaded")
+}
